c321148c61d066ba55db7053c1b17c8bce486e25
Vivesolav <147495594+Vivesolav@users.noreply.github.com>
2024-12-30 07:32:04 +0100

    Made the file: insert questions

---
+++ database/insert_questions.py
@@ -0,0 +1,17 @@
+INSERT INTO questions (question_text, person)
+VALUES ('What is your firstname?', 'advantage');
+
+INSERT INTO questions (question_text, person)
+VALUES ('What is your secondname?', 'advantage');
+
+INSERT INTO questions (question_text, person)
+VALUES ('What did you study?', 'advantage');
+
+INSERT INTO questions (question_text, person)
+VALUES ('What is your current job?', 'advantage');
+
+INSERT INTO questions (question_text, category) 
+VALUES ('Why would you choose a desktop over a laptop?', 'advantage');
+
+INSERT INTO questions (question_text, category) 
+VALUES ('What are the disadvantages of using a desktop?', 'disadvantage');
